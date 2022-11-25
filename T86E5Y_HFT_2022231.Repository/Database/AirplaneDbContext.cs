601358617c36389440cdd22e22b0e4485836a523
tapodianita <tapodianita@stud.uni-obuda.hu>
2022-11-25 07:45:34 +0100

    nuget packages added version 5.0.14

---
+++ T86E5Y_HFT_2022231.Repository/Database/AirplaneDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T86E5Y_HFT_2022231.Models.Entities;
+
+namespace T86E5Y_HFT_2022231.Repository.Database
+{
+  public class AirPlaneDbContext : DbContext
+  {
+
+  }
+}
