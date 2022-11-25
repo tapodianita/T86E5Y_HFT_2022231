9796f015c8e09f4f7059ef31ae0300cb87b8565f
tapodianita <tapodianita@stud.uni-obuda.hu>
2022-11-25 08:10:16 +0100

    dtos created

---
+++ T86E5Y_HFT_2022231.Models/DTOs/ManufacturerPlaneInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T86E5Y_HFT_2022231.Models.Entities;
+
+namespace T86E5Y_HFT_2022231.Models.DTOs
+{
+  public class ManufacturerPlaneInfo
+  {
+    public Manufacturer Manufacturer { get; set; }
+    public int AllPlane { get; set; }
+    public double AvgToAllPlanePercent { get; set; }
+    public override string ToString()
+    {
+      return $"Manufacturer = {Manufacturer.Name}, AllPlane = {AllPlane}, AvgToAllPlanePercent = {AvgToAllPlanePercent}%";
+    }
+  }
+}
