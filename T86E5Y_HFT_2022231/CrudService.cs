9ff7ce7cd80bab7405ffff1c7f3c293eab58d464
tapodianita <tapodianita@stud.uni-obuda.hu>
2022-11-25 09:13:50 +0100

    frontend/client rest service added

---
+++ T86E5Y_HFT_2022231/CrudService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T86E5Y_HFT_2022231.Client
+{
+  class CrudService
+  {
+  }
+}
