7e3e444336361aaaa54828597546e84ba3666c51
tapodianita <tapodianita@stud.uni-obuda.hu>
2022-11-25 08:15:58 +0100

    logic repositories created

---
+++ T86E5Y_HFT_2022231.Logic/Interfaces/IAirplaneLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T86E5Y_HFT_2022231.Models.DTOs;
+using T86E5Y_HFT_2022231.Models.Entities;
+
+namespace T86E5Y_HFT_2022231.Logic.Interfaces
+{
+  public interface IAirplaneLogic
+  {
+    void Create(Airplane item);
+    void Delete(int id);
+    Airplane Read(int id);
+    IQueryable<Airplane> ReadAll();
+    void Update(Airplane item);
+    IEnumerable<ManufacturerByYearInfo> ManufacturerByYearStatics();
+  }
+}
---
+++ T86E5Y_HFT_2022231.Logic/Interfaces/IFlightsLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T86E5Y_HFT_2022231.Models.DTOs;
+using T86E5Y_HFT_2022231.Models.Entities;
+
+namespace T86E5Y_HFT_2022231.Logic.Interfaces
+{
+  public interface IFlightsLogic
+  {
+    void Create(Flights item);
+    void Delete(int id);
+    Flights Read(int id);
+    IQueryable<Flights> ReadAll();
+    void Update(Flights item);
+  }
+}
